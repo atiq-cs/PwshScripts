67d7315210fb1bea2eb6d06f0257d080d74a886a
Atiq Rahman <atiqcx@gmail.com>
2019-11-30 18:21:12 -0800

    init script added wiki minor updated

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,31 +4,32 @@ Initialize pwsh environment
 .DESCRIPTION
 Provide highly optimized methods.
 
+Requires following Env Vars to be defined,
+- `$Env:PwshScriptDir`
+
 .EXAMPLE
-
+N/A
 .NOTES
-
+Eliminiates following vars,
+- PHOST
+- SC_DIR
 #>
 
 # get the last part of path, consumed by method: `prompt`
-function get-diralias ([string] $loc) {
-  # check if we are in our home script dir; yes: return grave sign
-  if ($loc.Equals($Env:PS_SC_DIR)) { return "~" }
+function get-diralias([string] $location) {
+  # check if we are in our home script dir; yes: return home sign, unix retro
+  if ($location.Equals($Env:PwshScriptDir)) { return "~" }
     
   # if it ends with \ that means we are in root of drive
   # in that case return drive
-  if ($loc.EndsWith("\")) { return $loc.Substring(0, $loc.Length-1) }
-  # for ref
-  #if (($lastindex = [int] $loc.lastindexof('\')) -ne -1) { return $loc.Substring(0, $lastindex) }
-    
+  if ($location.EndsWith("\")) { return $location.Substring(0, $location.Length-1) }
+
   # Otherwise return only the dir name
-  $lastindex = [int] $loc.lastindexof('\') + 1
-  return $loc.Substring($lastindex)
+  $lastindex = [int] $location.lastindexof('\') + 1
+  return $location.Substring($lastindex)
 }
 
 # Set prompt
 function prompt {
-    return "[atiq@" + $(If ($Env:IsWorkMachine -eq 'True') { 'fb' } Else { 'matrix' }) + " $(get-diralias($(get-location)))]$ "
+    return "[atiq@" + $(If ($Env:IsWorkMachine -eq 'True') { 'fb' } Else { 'matrix' }) + " $(get-diralias($(get-location))]$ "
 }
-
-$Env:PS_SC_DIR = 'D:\pwsh-scripts'
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## pwsh scripts project
-Most documentation is at wkiki,
+Most documentation is at [wiki](https://github.com/atiq-cs/pwsh-scripts/wiki),
 
 - [Deprecations](https://github.com/atiq-cs/pwsh-scripts/wiki/Miscellaneous-Deprecation)
 
