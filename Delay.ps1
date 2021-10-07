0c9c62aaaa65955e8dac38d1aa7749c7baf0c69d
Atiq Rahman <atiqcx@gmail.com>
2021-10-07 13:18:43 -0700

    Supports calling the script from any location (specify Home Dir) Update comments a bit

--- Delay.ps1
+++ Delay.ps1
@@ -1,40 +1,48 @@
+# Copyright (c) iQubit Inc.
+
 <#
 .SYNOPSIS
 Perform specified delay
 .DESCRIPTION
+Perform delay
+Display timer
+Show Notification
 ToDo: later, may be, update this with a C# implementation along with a binary
 
 .PARAMETER Time
-how long to delay and show a message
+how long to delay
 .EXAMPLE
  $ mdelay.ps1 2 "hello world"
  $ mdelay.ps1 5
 
 .NOTES
-Author: Atiq Rahman
-Date: 2011-11-15
+Utilized by Bluetooth.ps1
 #>
 
 [CmdletBinding()] Param (
   [Parameter(Mandatory=$true)] [int] $Time,
-  [string] $Msg)
+  [string] $Msg,
+  [string] $PwshScriptDir = 'D:\pwsh-scripts')
 
 
-<# Main function
-Refs,
-  https://technet.microsoft.com/en-us/library/ff730952.aspx
- Icon,
-  https://msdn.microsoft.com/en-us/library/system.windows.forms.tooltipicon.aspx
- NotifyIcon.ShowBalloonTip Method (Int32)
-  https://msdn.microsoft.com/en-us/library/ms160064.aspx
- Dispose
-  http://techibee.com/powershell/system-tray-pop-up-message-notifications-using-powershell/1865
-Component.Dispose Method ()
-  https://msdn.microsoft.com/en-us/library/3cc9y48w.aspx
+<#
+.SYNOPSIS
+  Main function
+.DESCRIPTION
+  References
+  - https://technet.microsoft.com/en-us/library/ff730952.aspx
+  Icon,
+  - https://msdn.microsoft.com/en-us/library/system.windows.forms.tooltipicon.aspx
+  NotifyIcon.ShowBalloonTip Method (Int32)
+  - https://msdn.microsoft.com/en-us/library/ms160064.aspx
+  Dispose
+  - http://techibee.com/powershell/system-tray-pop-up-message-notifications-using-powershell/1865
+  Component.Dispose Method ()
+  - https://msdn.microsoft.com/en-us/library/3cc9y48w.aspx
 #>
 function Main() {
-  # Assuming location of delay binary is in Env Path Var 
-  bin\Delay.exe $Time
+  # Retrieving $PwshScriptDir from Params
+  & ($PwshScriptDir + '\bin\Delay.exe') $Time
   # Start-Sleep $Time
 
   if ([string]::IsNullOrEmpty($Msg)) {
@@ -47,11 +55,6 @@ function Main() {
   [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
 
   $objNotifyIcon = New-Object System.Windows.Forms.NotifyIcon
-
-  # temp hack: variables are not inherited
-  If ([string]::IsNullOrEmpty($PwshScriptDir)) {
-    $PwshScriptDir = 'D:\pwsh-scripts'
-  }
 
   $objNotifyIcon.Icon = $PwshScriptDir + "\bin\mdelay_ps_icon.ico"
   $objNotifyIcon.BalloonTipIcon = "Info"
