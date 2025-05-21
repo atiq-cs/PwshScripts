b9c9a3b35e818a4e3d82f87daa102e99a333cab9
Atiq Rahman <atiqcx@gmail.com>
2025-05-21 14:28:36 -0700

    ArrayFi for consistent '-Match' behavior and  - make the args lower case i.e.,    - connect instead of Connect    - keyboard instead of Keyboard
    Fixes for bugs that occur when only 'one bluetooth device is connected'
     and when 'No bluetooth device is connected'
    
    as well as provide enhancements to 'Connected Devices List'
    - provide numbering

--- BT.ps1
+++ BT.ps1
@@ -11,19 +11,32 @@
   which device is the target?
 
 .EXAMPLE
-  BT.ps1 Disconnect Headset
-  BT.ps1 Connect Keyboard
+  BT.ps1 disconnect headset
+  BT.ps1 connect keyboard
 
 .NOTES
  Device names are hard coded in first block of main method..
  Linux only since `bluetoothctl` is required.
 
+  Arrayfi, dup code could be moved to a method utilizing 'Invoke-Expression'
+
+  $btclCmd = "bluetoothctl devices Paired"
+  Invoke-Expression $btclCmd
+
+  linux command i.e, 'bluetoothctl devices Paired' ideally returns an array of
+   strings. However, when it returns a single line it is just a string instead
+   of array of strings.
+  Hence, -Match returns
+   - System.Boolean when input is string
+   - returns MatchInfo when input is array of strings
+   one device is paired, next if statement will throw an exception
+
 tag: linux-only
 #>
 
 [CmdletBinding()] Param (
-  [Parameter(Mandatory=$true)] [ValidateSet('Connect', 'Disconnect', 'List')] [string] $Action,
-  [ValidateSet('Headset', 'Keyboard')] [string] $DeviceType = 'Keyboard')
+  [Parameter(Mandatory=$true)] [ValidateSet('connect', 'disconnect', 'list')] [string] $Action,
+  [ValidateSet('headset', 'keyboard')] [string] $DeviceType = 'keyboard')
 
 
 # Start of Main function
@@ -31,47 +44,78 @@ function Main() {
   # Set the headset as default since it is used most frequently these days
   $DeviceName = "TREBLAB Z7-Pro"
 
-  if ($DeviceType.Equals("Keyboard")) {
+  if ($DeviceType.Equals("keyboard")) {
     $DeviceName = "Keychron K8"
   }
-  elseif (-Not $DeviceType.Equals("Headset")) {
+  elseif (-Not $DeviceType.Equals("headset")) {
     Write-Host "Default device set to $DeviceName"
   }
 
   switch ($Action) {
-      "Connect" {
+      "connect" {
         if ((bluetoothctl devices Connected) -Match $DeviceName) {
-          Write-Host "Device already connected."
+          Write-Host "Device is already connected."
         } else {
-          # Need to run this in a loop for the keyboard
-          $matchedRowStr = (bluetoothctl devices Paired) -Match $DeviceName
-          if ($matchedRowStr) {
-            bluetoothctl connect $matchedRowStr.Split(' ')[1]
+          # Arrayfi comments above
+          $linesArray = bluetoothctl devices Paired
+          if ($linesArray.Count -Eq 1) {
+            $linesArray = @($linesArray)
+          }
+          $matchedRowStr = $linesArray -Match $DeviceName
+          if ($matchedRowStr.Length -Eq 1) {
+            # Need to run this in a loop for the keyboard
+            bluetoothctl connect $matchedRowStr[0].Split(' ')[1]
           } else {
             "Device: $DeviceName needs to be paired first!"
           }
         }
        }
-      "Disconnect" {
-        $matchedRowStr = (bluetoothctl devices Connected) -Match $DeviceName
-        if ($matchedRowStr) {
-          bluetoothctl disconnect $matchedRowStr.Split(' ')[1]
+      "disconnect" {
+        # Arrayfi bluetoothctl output so -Match behavior is consistent
+        $linesArray = bluetoothctl devices Connected
+        if ($linesArray.Count -Eq 1) {
+          $linesArray = @($linesArray)
+        }
+        $matchedRowStr = $linesArray -Match $DeviceName
+
+        if ($matchedRowStr.Length -Eq 1) {
+          bluetoothctl disconnect $matchedRowStr[0].Split(' ')[1]
         } else {
-          Write-Host "Device already disconnected."
+          Write-Host "Device is already disconnected."
         }
       }
-      "List" {
-        'Connected Devices'
-        '-----------------'
-        bluetoothctl devices Connected | ForEach-Object {
+      "list" {
+        $bCtlOutput = bluetoothctl devices Connected
+
+        if (-Not $bCtlOutput) {
+          'No bluetooth device is connected.'
+          return 
+        }
+
+        if ($bCtlOutput.Contains("Invalid")) {
+          Write-Host -ForegroundColor Red "bluetoothctl error: $bCtlOutput!"
+          return
+        }
+        
+        'Connected Devices List'
+        '----------------------'
+        $count = 1
+        ForEach ($line in $bCtlOutput) {
           # second argument to limit number of tokens
-          $_.Split(' ', 3)[2]
+          $deviceName = $line.Split(' ', 3)[2]
+          if ($deviceName) {
+            "$count. $deviceName"
+            $count++
+          }
+          else {
+            Write-Host -ForegroundColor Red "Error: $deviceName is empty!"
+          }
         }
       }
       default {
         'Invalid command line argument: ' + $Action
         return
-      }  
+      }
   }
 }
 
