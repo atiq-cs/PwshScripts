2d1fa75715e777c74867c0b8b602658eacd56ffe
Atiq Rahman <atiqcx@gmail.com>
2025-06-25 15:30:22 -0700

    Implement support category of tax files: State - only state supported right now is California / FTB - Utilize `ValidateScript` instead of `ValidateSet` to enable sophisticated validation of two types: Federal, State
    Additional Federal forms supported
    - f4952
    
    And, minor comment updates and example added for state

--- TaxTool.ps1
+++ TaxTool.ps1
@@ -1,43 +1,106 @@
 <#
 .SYNOPSIS
-  Download IRS Form given form number
+  Tax forms helper for Federal, State forms
+
 .DESCRIPTION
+  Download IRS/FTB Forms given form number
+   Open online instructions URL (TODO)
   Download the form and make a copy under ./forms directory (set via FormsBackupDir variable)
 
+  State default is California: Franchise Tax Board, for now
+
+.PARAMETER COType
+  Organization / company type: Federal (IRS), FTB (California)
+
 .PARAMETER FormName
-  IRS Form#
+  IRS / FTB Form#
 
 .PARAMETER OutputDir
-  Where to save
+  Where to save forms and to create back up dir inside
 
 .EXAMPLE
-  Get-IRSForm.ps1 1040sd ~/Doc/Tax-24
+  TaxTool.ps1 federal 1040sd ~/Doc/Tax-24
+  TaxTool.ps1 state 540 ~/Doc/Tax-24/ftb
 
 .NOTES
   
-Instruction URLs
-- 1040 Main: https://www.irs.gov/instructions/i1040gi (includes instructions for Sched 1, Sched 2
+Federal Instruction URLs
+- IRS EP    : https://www.irs.gov/instructions
+- 1040 Main : https://www.irs.gov/instructions/i1040gi (includes instructions for Sched 1, Sched 2
     and Sched 3)
 - 1040 Sch A: https://www.irs.gov/instructions/i1040sca
 - 1040 Sch D: https://www.irs.gov/instructions/i1040sd
 - 843       : https://www.irs.gov/instructions/i843
+
+FTB Instruction URLs
+Forms under Misc: https://www.ftb.ca.gov/forms/misc/XXXX.pdf
+
+- 540     : https://www.ftb.ca.gov/forms/2024/2024-540-booklet.html
+- 540-CA  : https://www.ftb.ca.gov/forms/2024/2024-540-ca-instructions.html
+ISR
+- 3853    : https://www.ftb.ca.gov/forms/2024/2024-3853-instructions.pdf
+Misc forms: 2917, 3701
 
 
 Demonstrates Invoke-WebRequest error handling
 #>
 
 [CmdletBinding()] Param (
+  [Parameter(Mandatory)] [ValidateSet('federal', 'state')]
+    [string] $COType,
+
   [Parameter(Mandatory=$true)]
-   [ValidateSet('1040sa', '1040sd', '1040s1', '1040s2', '1040s3', '6781', '8949', '843')]
-     [string] $FormName,
-  [Parameter(Mandatory=$true)] [string] $OutputDir)
+  #  [ValidateSet('1040sa', '1040sd', '1040s1', '1040s2', '1040s3', '6781', '8949', '843')]
+  #   [string] $FormName,
+  [ValidateScript({
+    $federalForms = '1040sa', '1040sd', '1040s1', '1040s2', '1040s3', '6781', `
+     '8949', '4952', '843'
+    $stateForms = '540', '540-ca', '3853', '2917', '3701'
+
+    If ($COType -Eq 'federal' -And $federalForms -contains $_) { return $true }
+    Elseif ($COType -Eq 'state' -And $stateForms -contains $_) { return $true }
+    Else {
+      throw "Invalid FormName `$_` for Type `$COType`. Valid values are: " +
+        ($COType -Eq 'federal' ? $federalForms -join ', ' : $stateForms -join ', ')
+    }
+  })]
+  [string] $FormName,
+
+  [Parameter(Mandatory=$true)]
+    [string] $OutputDir
+)
 
 
 function Main() {
   $FormsBackupDir = 'forms'
-  $IRSBaseURL = 'https://www.irs.gov/pub/irs-pdf/'
+  $CoBaseURL = 'https://www.irs.gov/pub/irs-pdf/'
+
   $fileName = 'f' + $FormName + '.pdf'
-  $Url = $IRSBaseURL + $fileName
+  if ($COType.Equals('state')) {
+    # `CoBaseURL` Initialization stuff
+    $CoBaseURL = 'https://www.ftb.ca.gov/forms/'
+
+    $IsMiscForm = [Boolean] $False
+    if ($FormName.Equals('2917') -Or $FormName.Equals('3701')) {
+      $IsMiscForm = $True
+    }
+
+    $TaxYear = [string] (Get-Date).AddYears(-1).Year
+    
+    if (-Not $IsMiscForm) {
+      $fileName = $TaxYear + '/' + $TaxYear + '-' + $FormName + '.pdf'
+    }
+    else {
+      $CoBaseURL = 'https://www.ftb.ca.gov/forms/misc/'
+      $fileName = $FormName + '.pdf'
+    }
+
+    $Url = $CoBaseURL + $fileName
+    $fileName = $FormName + '.pdf'
+  }
+  else {
+    $Url = $CoBaseURL + $fileName
+  }
 
   # Following Validation since addition of ValidateSet
   # if (-Not ($Url.EndsWith(".pdf"))) {
@@ -54,6 +117,9 @@ function Main() {
   #   break
   # }
   $bakFormName = 'f' + $FormName + '_orig.pdf'
+  if ($COType.Equals('state')) {
+    $bakFormName = $FormName + '_orig.pdf'
+  }
 
   Push-Location $OutputDir
   if (Test-Path $FormsBackupDir/$bakFormName) {
@@ -79,7 +145,7 @@ function Main() {
     }
     catch {
       # Handle errors, such as 404 (file not found)
-      if ($_.Exception.Response.StatusCode -eq 404) {
+      if ($_.Exception.Response.StatusCode -Eq 404) {
           Write-Host "Error: File not found (404). Check the URL."
       }
       else {
